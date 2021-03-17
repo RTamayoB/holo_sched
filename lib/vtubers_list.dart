c198061eaaf7bc46d98d9286dc2f86fe88cdd4e5
RafaelTB <rtamayo.mis@outlook.com>
2021-03-16 21:41:48 -0600

    Complete BranchList functionality, chosed ListTile is set to top and mock up vtuber names are shown, clicking again puts selected branch back to position.

--- lib/vtubers_list.dart
+++ lib/vtubers_list.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class BranchList extends StatelessWidget {
 
+  HeroController _heroController = HeroController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +17,7 @@ class BranchList extends StatelessWidget {
           },
         ),
         actions: [
-          CircleAvatar(
-          ),
+          CircleAvatar(),
         ],
       ),
       body: Column(
@@ -25,29 +26,28 @@ class BranchList extends StatelessWidget {
             padding: const EdgeInsets.all(4.0),
             child: TextField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.search),
-                hintText: 'Talent Name'
-              ),
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Talent Name'),
             ),
           ),
-          VtuberList()
+          Expanded(
+            child: Navigator(
+              observers: [_heroController],
+              onGenerateRoute: (RouteSettings settings) {
+                return MaterialPageRoute(
+                  builder: (_) => VtuberList(),
+                );
+              },
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: ''
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: ''
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: ''
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
         ],
         currentIndex: 1,
       ),
@@ -56,35 +56,20 @@ class BranchList extends StatelessWidget {
 }
 
 class VtuberList extends StatefulWidget {
-
   @override
   _VtuberListState createState() => _VtuberListState();
 }
 
 class _VtuberListState extends State<VtuberList> {
-
   //Temporary Vtuber List
   // TODO: Change this list for a firebase or similiar implementation.
   List<ListItem<String>> _kHololiveBranchs;
 
-
-
-  //Mockup data of vtubers for testing
-  final List<String> _kVutbers =  [
-    //Holo ID
-    "Ayunda Risu",
-    'Moona Hoshinova',
-    'Airani Iofifteen',
-    'Kureiji Ollie',
-    'Anya Melfissa',
-    'Pavolia Reine'
-  ];
-
-  void _showBranch(int index){
+  void _showBranch(int index) {
     print("Value of visible ${_kHololiveBranchs[index].isVisible}");
-    if(_kHololiveBranchs[index].isVisible == true) {
+    if (_kHololiveBranchs[index].isVisible == true) {
       _kHololiveBranchs[index].isVisible = false;
-    }else{
+    } else {
       _kHololiveBranchs[index].isVisible = true;
     }
   }
@@ -100,10 +85,8 @@ class _VtuberListState extends State<VtuberList> {
     _kHololiveBranchs.add(ListItem<String>("HOLO EN"));
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     return Expanded(
       child: ListView.builder(
         scrollDirection: Axis.vertical,
@@ -117,39 +100,135 @@ class _VtuberListState extends State<VtuberList> {
   Widget _vtuberListBuilder(BuildContext context, int index) {
     return Column(
       children: [
-        Container(
-          margin: EdgeInsets.all(4.0),
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Colors.green),
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            color: Colors.lightGreen,
-          ),
-          child: ListTile(
-            onTap: () {
-              // TODO: Display talents from branch
-              setState(() {
-                _showBranch(index);
-              });
-              print('${_kHololiveBranchs[index].data} pressed, displaying talents, visible is ${_kHololiveBranchs[index].isVisible}');
-            },
-            title: Text(
-              '${_kHololiveBranchs[index].data}',
-              style: Theme.of(context).textTheme.headline3,
+        Hero(
+          tag: _kHololiveBranchs[index].data,
+          child: Container(
+            margin: EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.green),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              color: Colors.lightGreen,
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.arrow_drop_down,),
+            child: ListTile(
+              onTap: () {
+                // TODO: Display talents from branch
+                setState(() {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return VtuberNames(_kHololiveBranchs[index].data);
+                  }));
+
+                  //_showBranch(index);
+                });
+                print(
+                    '${_kHololiveBranchs[index]
+                        .data} pressed, displaying talents, visible is ${_kHololiveBranchs[index]
+                        .isVisible}');
+              },
+              title: Text(
+                '${_kHololiveBranchs[index].data}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline3,
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                ),
+              ),
             ),
           ),
         ),
-        _kHololiveBranchs[index].isVisible ? Container() : Text("Shit"),
+        Hero(
+          tag: _kHololiveBranchs[index].data+"list",
+          child: Column(),
+        )
       ],
     );
   }
-
 }
 
 class ListItem<T> {
   bool isVisible = false; //Selection property to highlight or not
   T data; //Data of the user
   ListItem(this.data); //Constructor to assign the data
+}
+
+class VtuberNames extends StatelessWidget {
+  String branchName;
+
+  VtuberNames(String branch) {
+    this.branchName = branch;
+  }
+
+  //Mockup data of vtubers for testing
+  final List<String> _kVutbers = [
+    //Holo ID
+    "Ayunda Risu",
+    'Moona Hoshinova',
+    'Airani Iofifteen',
+    'Kureiji Ollie',
+    'Anya Melfissa',
+    'Pavolia Reine',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Hero(
+          tag: branchName,
+          child: Container(
+            margin: EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.green),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              color: Colors.lightGreen,
+            ),
+            child: ListTile(
+              onTap: () {
+                // TODO: Display talents from branch
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return VtuberList();
+                }));
+              },
+              title: Text(
+                branchName,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline3,
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Hero(
+          tag: branchName + "list",
+          child: Expanded(
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: _kVutbers.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2.0, color: Colors.orange),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      color: Colors.deepOrange,
+                    ),
+                    child: ListTile(
+                      title: Text(_kVutbers[index]),
+                    ),
+                  );
+                }),
+          ),
+        ),
+      ],
+    );
+  }
 }
