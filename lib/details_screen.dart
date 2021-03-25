a9f934fc5f9013040ea58f47a45942e5bed210f6
RTamayoB <rtamayo.mis@outlook.com>
2021-03-25 00:30:06 -0600

    Created basic details_screen.dart

---
+++ lib/details_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  final HeroController _heroController = HeroController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HoloSched'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Menu Icon',
+          onPressed: () {
+            print('Icon Menu pressed');
+          },
+        ),
+        actions: [
+          CircleAvatar(),
+        ],
+      ),
+      // TODO: Add body with detail page
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
+        ],
+        currentIndex: 1,
+      ),
+    );
+  }
+}
