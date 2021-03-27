88ec6e8f189d1d1ae28cea9c8a9cb91b8f61f834
RTamayoB <rtamayo.mis@outlook.com>
2021-03-27 15:38:07 -0600

    Created base_screen.dart Added tab views

---
+++ lib/base_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> with SingleTickerProviderStateMixin, RestorationMixin {
+  TabController _tabController;
+
+  final RestorableInt tabIndex = RestorableInt(0);
+
+  @override
+  String get restorationId => 'tab_non_scrollable';
+
+  @override
+  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 2,
+      vsync: this,
+    );
+    _tabController.addListener(() {
+      // When the tab controller's value is updated, make sure to update the
+      // tab index value, which is state restorable.
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final tabs = [
+      'Talents',
+      'Favorites'
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HoloSched'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Menu Icon',
+          onPressed: () {
+            print('Icon Menu pressed');
+          },
+        ),
+        actions: [
+          CircleAvatar(),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            for(final tab in tabs) Tab(text: tab,),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          for(final tab in tabs)
+            Center(
+              child: Text(tab),
+            )
+        ],
+      ),
+      //Enable bottom navigation instead of tabbed view when more than 2 tabs
+      /*
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
+        ],
+        currentIndex: 1,
+      ),*/
+    );
+  }
+}
